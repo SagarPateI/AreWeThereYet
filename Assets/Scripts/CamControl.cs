38ff3ba217f38e4fc801da89c49f9fc14939db91
Atrollingston <3olympus03@gmail.com>
2024-01-27 11:27:12 -0500

    Triggering Attempt

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class CamControl : MonoBehaviour
 {
-    public Transform bckg1;
-    public Transform bckg2;
+    public GameObject Back1;
+    public GameObject Back2;
     public float ms;
-    public Transform target;
+    float randomizer;
 
     private bool didstart = true;
 
@@ -16,33 +16,15 @@ public class CamControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        size = bckg1.GetComponent<BoxCollider2D>().size.y;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        if (transform.position.y >= bckg2.position.y)
-        {
-            bckg1.position = new Vector3(bckg1.position.x, bckg2.position.y + size, bckg1.position.z);
-            SwitchBckg();
-        }
-
-        if (transform.position.y < bckg1.position.y)
-        {
-            bckg2.position = new Vector3(bckg2.position.x, bckg1.position.y - size, bckg2.position.z);
-            SwitchBckg();
-        }
         if(didstart == true)
         {
             transform.Translate(Vector3.up * ms * Time.deltaTime);
         }
-    }
-    private void SwitchBckg()
-    {
-        Transform temp = bckg1;
-        bckg1 = bckg2;
-        bckg2 = temp;
     }
 }
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject primary;
+    private CamControl camscript;
+    float randomizer;
+    Transform spawnlocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        primary = GameObject.Find("Main Camera");
+        camscript = primary.GetComponent<CamControl>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        randomizer = Random.Range(0, 1);
+        spawnlocation = gameObject.transform;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered");
+        if(randomizer == 0)
+        {
+            Object.Instantiate(camscript.Back1, new Vector3(0, spawnlocation.position.y, 0), transform.rotation);
+        }
+        if (randomizer == 0)
+        {
+            Object.Instantiate(camscript.Back2, new Vector3(0, spawnlocation.position.y, 0), transform.rotation);
+        }
+    }
+}
