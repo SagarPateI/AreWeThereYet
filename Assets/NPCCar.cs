706351efcef5aa7ea72144474c05d600cf54f70e
Atrollingston <3olympus03@gmail.com>
2024-01-27 22:19:42 -0500

    Cars Work now

--- Assets/NPCCar.cs
+++ Assets/NPCCar.cs
@@ -1,6 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCCar : MonoBehaviour
+public class NPCCar: MonoBehaviour
 {
+    public GameObject patienceMeter;
+    private PatienceMeter patienceMeterCode;
+    public float speed = 5f;
 
+    void Start()
+    {
+        patienceMeterCode = patienceMeter.GetComponent<PatienceMeter>();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == ("Player"))
+        {
+            Debug.Log("Player hit an NPC car!");
+
+            if (patienceMeter != null)
+            {
+                Debug.Log("subtracted patience!");
+                patienceMeterCode.DecreasePatienceByHit();
+            }
+        }
+    }
 }
