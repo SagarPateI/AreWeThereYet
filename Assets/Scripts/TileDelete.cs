7850d030db9da807389e370ac57f541d2b1b67c9
Atrollingston <3olympus03@gmail.com>
2024-01-28 15:01:14 -0500

    Tiles don't despawn until car starts

--- Assets/Scripts/TileDelete.cs
+++ Assets/Scripts/TileDelete.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class TileDelete : MonoBehaviour
 {
+    public GameObject camController;
+    private bool started = false;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 20f);
+        camController = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (camController.GetComponent<CamControl>().didstart)
+        {
+            started = true;
+        }
+        if(started)
+        {
+            Destroy(gameObject, 20f);
+        }
     }
 }
