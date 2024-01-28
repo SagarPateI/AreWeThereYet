4fd591dda7ae83ccd7d0ef7083907a1e5b907e00
Spade <47337405+SagarPateI@users.noreply.github.com>
2024-01-27 19:01:18 -0500

    Modified PatienceMeter to properly add or subtract from the meter

--- Assets/Scripts/PatienceMeter.cs
+++ Assets/Scripts/PatienceMeter.cs
@@ -1,45 +1,37 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
 
 public class PatienceMeter : MonoBehaviour
 {
+    private Slider slider;
+    public float FillSpeed = 0.5f;
     public float startingPatience = 100f;
     public float hitPenalty = 5f;
     public float stfuPenalty = 10f;
-    // the screen prompting player switch
     public GameObject switchPlayerScreen;
-    // the UI slider representing patience meter
-    public Slider patienceSlider; 
-    private float currentPatience;
+    private float targetProgress;
     public GameObject scoreManager;
 
     void Start()
     {
-        currentPatience = startingPatience;
+        slider = GetComponent<Slider>();
+        targetProgress = startingPatience;
         UpdatePatienceUI();
     }
 
-    // decrease patience when the car hits an object
-    public void DecreasePatienceByHit()
+    void Update()
     {
-        currentPatience -= hitPenalty;
-        CheckPatience();
-        UpdatePatienceUI();
-    }
+        if (slider.value < targetProgress)
+        {
+            slider.value += FillSpeed * Time.deltaTime;
+        }
+        else if (slider.value > targetProgress)
+        {
+            slider.value -= FillSpeed * Time.deltaTime;
+        }
 
-    // decrease patience when the babysitter tells the kid to STFU
-    public void DecreasePatienceBySTFU()
-    {
-        currentPatience -= stfuPenalty;
-        CheckPatience();
-        UpdatePatienceUI();
-    }
-
-    // check if patience has reached zero
-    void CheckPatience()
-    {
-        if (currentPatience <= 0)
+        // check if patience has reached zero
+        if (slider.value <= 0)
         {
             scoreManager.GetComponent<ScoreScript>().SaveScore();
             switchPlayerScreen.SetActive(true);
@@ -47,22 +39,28 @@ public class PatienceMeter : MonoBehaviour
         }
     }
 
-    //Used for Score. Returns True/False depending on if patience is 0 or not
-    public bool isPatienceZero()
+    // decrease patience when the car hits an object
+    public void DecreasePatienceByHit()
     {
-        if(currentPatience <= 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        targetProgress -= hitPenalty;
+    }
+
+    // decrease patience when the babysitter tells the kid to STFU
+    public void DecreasePatienceBySTFU()
+    {
+        targetProgress -= stfuPenalty;
     }
 
     // update the UI slider to reflect current patience value
     void UpdatePatienceUI()
     {
-        patienceSlider.value = currentPatience / startingPatience;
+        slider.value = targetProgress / startingPatience;
+    }
+
+    // used for Score
+    // returns true/false depending on if patience is 0 or not
+    public bool isPatienceZero()
+    {
+        return targetProgress <= 0;
     }
 }
